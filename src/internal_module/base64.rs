6d446704b02cc62aacd41fc8eee24e56087702a5
Jovi De Croock <decroockjovi@gmail.com>
2023-10-31 10:13:42 +0100

    add coerce step

--- src/internal_module/base64.rs
+++ src/internal_module/base64.rs
@@ -4,56 +4,72 @@ use base64::{Engine as _, engine::general_purpose};
 use crate::{JsValue, Context, JsString};
 use crate::quickjs_sys::*;
 
+fn coerce_to_string(ctx: &mut Context, js_value: &JsValue) -> Option<JsString> {
+  match js_value {
+    JsValue::String(value) => Some(value.to_owned()),
+    JsValue::Int(number) => Some(ctx.new_string(number.to_string().as_str())),
+    JsValue::Bool(bool) => Some(ctx.new_string(bool.to_string().as_str())),
+    JsValue::Float(float) => Some(ctx.new_string(float.to_string().as_str())),
+    JsValue::UnDefined => Some(ctx.new_string("undefined")),
+    JsValue::Null => Some(ctx.new_string("null")),
+    _ => None
+  }
+}
+
 fn atob(ctx: &mut Context, _this_val: JsValue, argv: &[JsValue]) -> JsValue {
     let base64_string = match argv.get(0) {
-      Some(JsValue::String(base64_string)) => Some(base64_string),
-      // For correctness we would need to convert any number, undefined or null to 
-      // a string but not sure if we can coerce here.
-      Some(_value) => {
-        ctx.throw_type_error("atob needs a string-argument to be passed");
-        return JsValue::UnDefined;
-      }
-      None => {
+      Some(JsValue::String(base64_string)) => base64_string.to_owned(),
+      _ => {
         ctx.throw_type_error("atob needs an argument to be passed");
         return JsValue::UnDefined;
       }
     };
-    if let Some(base64_string) = base64_string {
-      let result = general_purpose::STANDARD.decode(base64_string.to_string());
-      match result {
-        Ok(decoded) => {
-          let result = String::from_utf8(decoded);
-          match result {
-            Ok(final_decoded_string) => {
-              let js_string = ctx.new_string(final_decoded_string.as_str());
-              JsValue::String(js_string)
-            }
-            Err(_e) => {
-              ctx.throw_type_error("Could not decode to UTF-8 String");
-              JsValue::UnDefined
-            }
+
+    let result = general_purpose::STANDARD.decode(base64_string.to_string());
+    match result {
+      Ok(decoded) => {
+        let result = String::from_utf8(decoded);
+        match result {
+          Ok(final_decoded_string) => {
+            let js_string = ctx.new_string(final_decoded_string.as_str());
+            JsValue::String(js_string)
           }
-        }
-        Err(_e) => {
+          Err(_e) => {
             ctx.throw_type_error("Could not decode to UTF-8 String");
             JsValue::UnDefined
+          }
         }
       }
-    } else {
-        JsValue::UnDefined
+      Err(_e) => {
+          ctx.throw_type_error("Could not decode to UTF-8 String");
+          JsValue::UnDefined
+      }
     }
 }
 
 fn btoa(ctx: &mut Context, _this_val: JsValue, argv: &[JsValue]) -> JsValue {
-    let raw_string = argv.get(0);
-    if let Some(JsValue::String(raw_string)) = raw_string {
-      let encoded_string = general_purpose::STANDARD.encode(raw_string.to_string());
-      let js_string = ctx.new_string(encoded_string.as_str());
-      JsValue::String(js_string)
-    } else {
-      ctx.throw_type_error("atob needs a string argument to be passed");
+  let raw_string = match argv.get(0) {
+    Some(JsValue::String(base64_string)) => base64_string.to_owned(),
+    // For correctness we would need to convert any number, undefined or null to 
+    // a string but not sure if we can coerce here.
+    Some(value) => {
+      let result = coerce_to_string(ctx, value);
+      if let Some(result) = result {
+        result
+      } else {
+        ctx.throw_type_error("btoa needs a string-argument to be passed");
+        return JsValue::UnDefined;
+      }
+    }
+    None => {
+      ctx.throw_type_error("btoa needs an argument to be passed");
       return JsValue::UnDefined;
     }
+  };
+
+    let encoded_string = general_purpose::STANDARD.encode(raw_string.to_string());
+    let js_string = ctx.new_string(encoded_string.as_str());
+    JsValue::String(js_string)
 }
 
 pub fn init_base64_functions(ctx: &mut Context) {
