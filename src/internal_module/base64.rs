a5c6405efa45b18a834798986ac8524a8d1a90be
Jovi De Croock <decroockjovi@gmail.com>
2023-10-31 09:45:21 +0100

    convert types stuffies

--- src/internal_module/base64.rs
+++ src/internal_module/base64.rs
@@ -7,12 +7,12 @@ use crate::quickjs_sys::*;
 fn atob(ctx: &mut Context, _this_val: JsValue, argv: &[JsValue]) -> JsValue {
     let base64_string = argv.get(0);
     if let Some(JsValue::String(base64_string)) = base64_string {
-      let result = general_purpose::STANDARD_NO_PAD.decode(base64_string.into());
+      let result = general_purpose::STANDARD_NO_PAD.decode(base64_string.to_string());
       if let Ok(decoded) = result {
         let result = String::from_utf8(decoded);
         if let Ok(final_decoded_string) = result {
-          // TODO: needs to become JsString
-          JsValue::String(final_decoded_string.into())
+          let js_string = ctx.new_string(final_decoded_string.as_str());
+          JsValue::String(js_string)
         } else {
           JsValue::UnDefined
         }
@@ -27,14 +27,11 @@ fn atob(ctx: &mut Context, _this_val: JsValue, argv: &[JsValue]) -> JsValue {
 fn btoa(ctx: &mut Context, _this_val: JsValue, argv: &[JsValue]) -> JsValue {
     let raw_string = argv.get(0);
     if let Some(JsValue::String(raw_string)) = raw_string {
-      let result = general_purpose::STANDARD_NO_PAD.encode(raw_string.into());
-      if let Ok(encoded) = result {
-        JsValue::String(encoded)
-      } else {
-        JsValue::UnDefined
-      }
+      let encoded_string = general_purpose::STANDARD_NO_PAD.encode(raw_string.to_string());
+      let js_string = ctx.new_string(encoded_string.as_str());
+      JsValue::String(js_string)
     } else {
-        JsValue::UnDefined
+      JsValue::UnDefined
     }
 }
 
